003e97f934448d195d32ae677417db0a98974d5f
Jesus C Garza <55368712+Zabato@users.noreply.github.com>
2020-05-26 12:36:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-//generate password
-function generate(){
-    //set password length 
-    let complexity = document.getElementById("slider").value;
-
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-    
-    let password = "";
-
-    //create for loop to choose password 
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-
-    //add password to display
-    document.getElementById("display").value = password;
-
-    //add password to previously generated passwod section 
-    document.getElementById("lastNums").innerHTML += password + "<br />";
-}
-
-//set default length display
-document.getElementById("length").innerHTML = "Length: 25";
-
-//function to set length based on slider postion
-document.getElementById("slider").oninput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value; 
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
-    }
-}
-//function to copy password to clipboard
-function copyPassword(){
-    document.getElementById("display").select();
-    
-    document.execCommand("Copy");
-
-    alert("Password copied to Clipboard!");
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>Password Generator</h1>
+      </header>
+      <div class="card">
+        <div class="card-header">
+          <h2>Generate a Password</h2>
+        </div>
+        <div class="card-body">
+          <textarea
+            readonly
+            id="password"
+            placeholder="Your Secure Password"
+            aria-label="Generated Password"
+          ></textarea>
+        </div>
+        <div class="card-footer">
+          <button id="generate" class="btn">Generate Password</button>
+        </div>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
